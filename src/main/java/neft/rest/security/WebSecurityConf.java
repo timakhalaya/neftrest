78fb99c1af7fab45ca9837b202a81baee4dbbbd8
tim.akhalaya <tim.akhalaya@gmail.com>
2021-03-11 14:31:00 +0100

    using SSO role based access with tokens

--- src/main/java/neft/rest/security/WebSecurityConf.java
+++ src/main/java/neft/rest/security/WebSecurityConf.java
@@ -1,23 +1,57 @@
 package neft.rest.security;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConf extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class WebSecurityConf extends KeycloakWebSecurityConfigurerAdapter {
+
   @Override
   public void configure(HttpSecurity http) throws Exception {
-    http
-      .csrf()
-      .disable()
-      .authorizeRequests()
+    super.configure(http);
+    http.authorizeRequests()
+      .antMatchers("/services/calculator/divide").hasAnyRole("user")
+      .antMatchers("/services/calculator/multiply").hasAnyRole("user")
+      .antMatchers("/services/calculator/subtract").hasAnyRole("user")
+      .antMatchers("/services/calculator/add").hasAnyRole("user")
       .anyRequest()
-      .authenticated()
-      .and()
-      .httpBasic();
+      .permitAll();
+    http.csrf().disable();
   }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+    keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+    auth.authenticationProvider(keycloakAuthenticationProvider);
+  }
+
+
+  @Bean
+  @Override
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+  }
+
+  @Bean
+  public KeycloakConfigResolver KeycloakConfigResolver() {
+    return new KeycloakSpringBootConfigResolver();
+  }
+
 }
 
